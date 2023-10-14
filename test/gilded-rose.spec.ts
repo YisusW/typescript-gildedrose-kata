a87b35a9a968673b4ad5d966497acf1dfcacf433
YisusW <jesusochoa266@gmail.com>
2023-10-14 17:48:12 -0300

    test: adding backstage case in unit test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -42,8 +42,10 @@ describe("gilded rose", () => {
 
     describe("Aged Brie", () => {
 
-        it("Aged Brie Increases Quality With Age", () => {
-            const agedBrie = new ItemClass("Aged Brie", 5, 1);
+        const NAME_AGED_BRIE = "Aged Brie";
+
+        it("Increases Quality With Age", () => {
+            const agedBrie = new ItemClass(NAME_AGED_BRIE, 5, 1);
     
             const gildedRose = new GildedRose([agedBrie]);
             gildedRose.updateQuality();
@@ -52,7 +54,7 @@ describe("gilded rose", () => {
         });
 
         it("Quality Never Increases Past Fifty", () => {
-            const agedBrie = new ItemClass("Aged Brie", 5, 50);
+            const agedBrie = new ItemClass(NAME_AGED_BRIE, 5, 50);
     
             const gildedRose = new GildedRose([agedBrie]);
             gildedRose.updateQuality();
@@ -61,4 +63,45 @@ describe("gilded rose", () => {
         });
 
     });
+
+    describe("Backstage", () => {
+        const NAME_BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+
+        it("Pass Increases Quality By One If Sell By Greater Then Ten", () => {
+            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 11, 20);
+    
+            const gildedRose = new GildedRose([backstagePasses]);
+            gildedRose.updateQuality();
+
+            expect(backstagePasses.quality).toBe(21);
+        });
+
+        it("Pass Increases Quality By Two If Sell By Smaller Than Ten", () => {
+            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 6, 20);
+    
+            const gildedRose = new GildedRose([backstagePasses]);
+            gildedRose.updateQuality();
+
+            expect(backstagePasses.quality).toBe(22);
+        });
+
+        it("Pass Increases Quality By Three If Sell By Smaller Than Five", () => {
+            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 5, 20);
+    
+            const gildedRose = new GildedRose([backstagePasses]);
+            gildedRose.updateQuality();
+
+            expect(backstagePasses.quality).toBe(23);
+        });
+
+        it("Pass Loses Value After Sell By Passes", () => {
+            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 0, 20);
+    
+            const gildedRose = new GildedRose([backstagePasses]);
+            gildedRose.updateQuality();
+
+            expect(backstagePasses.quality).toBe(0);
+        });
+    });
+
 });
