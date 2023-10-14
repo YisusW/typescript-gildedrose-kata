ae925f0ae7c3356ba22280e2c733c6e3e5c5c162
YisusW <jesusochoa266@gmail.com>
2023-10-14 17:24:15 -0300

    refactor: linting the source main components
    in addition linting the item class ts file with command in package scrips

--- src/item-class.ts
+++ src/item-class.ts
@@ -1,5 +1,9 @@
 import { Item } from "./item.interface";
 
 export class ItemClass implements Item {
-    constructor(public name: string, public sellIn: number, public quality: number) {}
+	constructor(
+		public name: string,
+		public sellIn: number,
+		public quality: number,
+	) {}
 }
