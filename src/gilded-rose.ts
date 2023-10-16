60fa36d8d14bb27c1726e1c56cbddce1eabfada1
YisusW <jesusochoa266@gmail.com>
2023-10-16 15:32:05 -0300

    lint: passing the lint action in CI and renamed the file

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -1,21 +1,20 @@
 import { Item } from "./item.interface";
 
 export class GildedRose {
-
 	private readonly AGED_BRIE = "Aged Brie";
-    private readonly BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private readonly SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private readonly BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private readonly SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    private readonly AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD = 0;
+	private readonly AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD = 0;
 
-    private readonly BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 10;
-    private readonly BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 5;
-    private readonly BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD = 0;
+	private readonly BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 10;
+	private readonly BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 5;
+	private readonly BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD = 0;
 
-    private readonly DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD = 0;
-    
-    private readonly MAX_QUALITY = 50;
-    private readonly MIN_QUALITY = 0;
+	private readonly DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD = 0;
+
+	private readonly MAX_QUALITY = 50;
+	private readonly MIN_QUALITY = 0;
 
 	constructor(public items: Item[]) {}
 
@@ -23,22 +22,21 @@ export class GildedRose {
 		const items = [...this.items];
 
 		items.forEach((item) => {
-
 			switch (item.name) {
-                case this.AGED_BRIE:
-                    this.decreaseSellIn(item);
-                    this.updateAgedBrieQuality(item);
-                    break;
-                case this.BACKSTAGE_PASSES:
-                    this.decreaseSellIn(item);
-                    this.updateBackstagePassesQuality(item);
-                    break;
-                case this.SULFURAS:
-                    break;
-                default:
-                    this.decreaseSellIn(item);
-                    this.updateDefaultItemQuality(item);
-                    break;
+				case this.AGED_BRIE:
+					this.decreaseSellIn(item);
+					this.updateAgedBrieQuality(item);
+					break;
+				case this.BACKSTAGE_PASSES:
+					this.decreaseSellIn(item);
+					this.updateBackstagePassesQuality(item);
+					break;
+				case this.SULFURAS:
+					break;
+				default:
+					this.decreaseSellIn(item);
+					this.updateDefaultItemQuality(item);
+					break;
 			}
 		});
 	}
@@ -48,50 +46,50 @@ export class GildedRose {
 	}
 
 	private updateAgedBrieQuality(item: Item) {
-        this.increaseQuality(item);
+		this.increaseQuality(item);
 
-        if (item.sellIn < this.AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD) {
-            this.increaseQuality(item);
-        }
-    }
+		if (item.sellIn < this.AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD) {
+			this.increaseQuality(item);
+		}
+	}
 
 	private updateBackstagePassesQuality(item: Item) {
-        this.increaseQuality(item);
+		this.increaseQuality(item);
 
-        if (item.sellIn < this.BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD) {
-            this.increaseQuality(item);
-        }
+		if (item.sellIn < this.BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD) {
+			this.increaseQuality(item);
+		}
 
-        if (item.sellIn < this.BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD) {
-            this.increaseQuality(item);
-        }
+		if (item.sellIn < this.BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD) {
+			this.increaseQuality(item);
+		}
 
-        if (item.sellIn < this.BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD) {
-            this.resetQuality(item);
-        }
-    }
+		if (item.sellIn < this.BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD) {
+			this.resetQuality(item);
+		}
+	}
 
 	private updateDefaultItemQuality(item: Item): void {
-        this.decreaseQuality(item);
+		this.decreaseQuality(item);
 
-        if (item.sellIn < this.DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD) {
-            this.decreaseQuality(item);
-        }
-    }
+		if (item.sellIn < this.DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD) {
+			this.decreaseQuality(item);
+		}
+	}
 
 	private resetQuality(item: Item) {
-        item.quality = 0;
-    }
+		item.quality = 0;
+	}
 
 	private increaseQuality(item: Item) {
-        if (item.quality < this.MAX_QUALITY) {
-            item.quality += 1;
-        }
-    }
+		if (item.quality < this.MAX_QUALITY) {
+			item.quality += 1;
+		}
+	}
 
 	private decreaseQuality(item: Item) {
-        if (item.quality > this.MIN_QUALITY) {
-            item.quality -= 1;
-        }
-    }
+		if (item.quality > this.MIN_QUALITY) {
+			item.quality -= 1;
+		}
+	}
 }
--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -40,79 +40,77 @@ describe("gilded rose", () => {
 		});
 	});
 
-    describe("Aged Brie", () => {
+	describe("Aged Brie", () => {
+		const NAME_AGED_BRIE = "Aged Brie";
 
-        const NAME_AGED_BRIE = "Aged Brie";
+		it("Increases Quality With Age", () => {
+			const agedBrie = new ItemClass(NAME_AGED_BRIE, 5, 1);
 
-        it("Increases Quality With Age", () => {
-            const agedBrie = new ItemClass(NAME_AGED_BRIE, 5, 1);
-    
-            const gildedRose = new GildedRose([agedBrie]);
-            gildedRose.updateQuality();
-    
-            expect(agedBrie.quality).toBe(2);
-        });
+			const gildedRose = new GildedRose([agedBrie]);
+			gildedRose.updateQuality();
 
-        it("Quality Never Increases Past Fifty", () => {
-            const agedBrie = new ItemClass(NAME_AGED_BRIE, 5, 50);
-    
-            const gildedRose = new GildedRose([agedBrie]);
-            gildedRose.updateQuality();
+			expect(agedBrie.quality).toBe(2);
+		});
 
-            expect(agedBrie.quality).toBe(50);
-        });
+		it("Quality Never Increases Past Fifty", () => {
+			const agedBrie = new ItemClass(NAME_AGED_BRIE, 5, 50);
 
-    });
+			const gildedRose = new GildedRose([agedBrie]);
+			gildedRose.updateQuality();
 
-    describe("Backstage", () => {
-        const NAME_BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+			expect(agedBrie.quality).toBe(50);
+		});
+	});
 
-        it("Pass Increases Quality By One If Sell By Greater Then Ten", () => {
-            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 11, 20);
-    
-            const gildedRose = new GildedRose([backstagePasses]);
-            gildedRose.updateQuality();
+	describe("Backstage", () => {
+		const NAME_BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 
-            expect(backstagePasses.quality).toBe(21);
-        });
+		it("Pass Increases Quality By One If Sell By Greater Then Ten", () => {
+			const backstagePasses = new ItemClass(NAME_BACKSTAGE, 11, 20);
 
-        it("Pass Increases Quality By Two If Sell By Smaller Than Ten", () => {
-            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 6, 20);
-    
-            const gildedRose = new GildedRose([backstagePasses]);
-            gildedRose.updateQuality();
+			const gildedRose = new GildedRose([backstagePasses]);
+			gildedRose.updateQuality();
 
-            expect(backstagePasses.quality).toBe(22);
-        });
+			expect(backstagePasses.quality).toBe(21);
+		});
 
-        it("Pass Increases Quality By Three If Sell By Smaller Than Five", () => {
-            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 5, 20);
-    
-            const gildedRose = new GildedRose([backstagePasses]);
-            gildedRose.updateQuality();
+		it("Pass Increases Quality By Two If Sell By Smaller Than Ten", () => {
+			const backstagePasses = new ItemClass(NAME_BACKSTAGE, 6, 20);
 
-            expect(backstagePasses.quality).toBe(23);
-        });
+			const gildedRose = new GildedRose([backstagePasses]);
+			gildedRose.updateQuality();
 
-        it("Pass Loses Value After Sell By Passes", () => {
-            const backstagePasses = new ItemClass(NAME_BACKSTAGE, 0, 20);
-    
-            const gildedRose = new GildedRose([backstagePasses]);
-            gildedRose.updateQuality();
+			expect(backstagePasses.quality).toBe(22);
+		});
 
-            expect(backstagePasses.quality).toBe(0);
-        });
-    });
+		it("Pass Increases Quality By Three If Sell By Smaller Than Five", () => {
+			const backstagePasses = new ItemClass(NAME_BACKSTAGE, 5, 20);
 
-    describe("Sulfuras", () => {
-        it("Never Changes", () => {
-            const sulfuras = new ItemClass("Sulfuras, Hand of Ragnaros", 0, 25);
-    
-            const gildedRose = new GildedRose([sulfuras]);
-            gildedRose.updateQuality();
+			const gildedRose = new GildedRose([backstagePasses]);
+			gildedRose.updateQuality();
 
-            expect(sulfuras.quality).toBe(25);
-            expect(sulfuras.sellIn).toBe(0);
-        });
-    });
+			expect(backstagePasses.quality).toBe(23);
+		});
+
+		it("Pass Loses Value After Sell By Passes", () => {
+			const backstagePasses = new ItemClass(NAME_BACKSTAGE, 0, 20);
+
+			const gildedRose = new GildedRose([backstagePasses]);
+			gildedRose.updateQuality();
+
+			expect(backstagePasses.quality).toBe(0);
+		});
+	});
+
+	describe("Sulfuras", () => {
+		it("Never Changes", () => {
+			const sulfuras = new ItemClass("Sulfuras, Hand of Ragnaros", 0, 25);
+
+			const gildedRose = new GildedRose([sulfuras]);
+			gildedRose.updateQuality();
+
+			expect(sulfuras.quality).toBe(25);
+			expect(sulfuras.sellIn).toBe(0);
+		});
+	});
 });
